2646488404dd84e16df65bcdb200851e15e697ae
shunhamm <shun.022885@gmail.com>
2023-04-12 12:50:29 +0900

    set the html templates(base, character_detail), and define view functions

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+
+    photo_list = {}
+    return HttpResponse(photo_list)
+
+def rules(request):
+    
+    game_rule = ""
+    return HttpResponse(game_rule)
+
+def notables_detail(request):
+
+    legend_list = {}    
+    return HttpResponse(legend_list)
+
+def notables_list(request):
+
+    legend_list = {}
+    return HttpResponse(legend_list)
+
+def external_links(request):
+
+    recommend_external_website = {}
+    return HttpResponse(recommend_external_website)
