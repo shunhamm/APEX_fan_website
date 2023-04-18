c7f34c6fb55eed3300865de45782f60a90917963
shunhamm <shun.022885@gmail.com>
2023-04-18 15:15:17 +0900

    set admin page info and complete to make index view

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Character(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    image_path = models.CharField(max_length=50)
+    image_path = models.CharField(max_length=100, null=True, blank=True)
--- game_fan_website/settings.py
+++ game_fan_website/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'game_fan_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
